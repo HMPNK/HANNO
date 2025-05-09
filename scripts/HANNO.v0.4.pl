0a7b2f23bcedd356036be9523fbc93826ce94ceb
HMPNK <51913753+HMPNK@users.noreply.github.com>
2025-05-09 11:37:11 +0200

    Update HANNO.v0.4.pl

--- scripts/HANNO.v0.4.pl
+++ scripts/HANNO.v0.4.pl
@@ -7,7 +7,7 @@
 
 ##define path to HANNO scripts and MAMBA installation (depend on your system, change before first run!)
 my $scr = "/data2/HANNO/scripts";
-my $mamba="/home/kuhl/miniconda2/envs/MAMBA/bin";
+my $mamba="/home/$USER/miniconda3/envs/MAMBA/bin";
 my $eggnog="/data2/HANNO/EGGNOGG-DBs";
 
 ##VARIABLES
